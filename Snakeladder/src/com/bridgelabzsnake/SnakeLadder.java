3bc189648641ea8039647f48d3533800b0c88579
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-07 13:28:19 +0530

    Added check for options noplay snake ladder

--- Snakeladder/src/com/bridgelabzsnake/SnakeLadder.java
+++ Snakeladder/src/com/bridgelabzsnake/SnakeLadder.java
@@ -6,13 +6,32 @@ package com.bridgelabzsnake;
 
 	public class SnakeLadder {
 
-	    public static void main(String[] args){
+		public static void main(String[] args){
 	        System.out.println("Welcome to Snake Ladder Program");
 	        Scanner read = new Scanner(System.in);
 	        int num = 0;
+	        int position;
 	        System.out.println("The position  number is at 0 ");
 	        Random random = new Random();
 	        int dice1 = random.nextInt(6)+1;
 	        System.out.println("The player rolls dice " +dice1);
+	        int noPlay=0 ,ladder=1,snake=2;
+	        int dice2 = random.nextInt(3); // the player checks condition for noplay,ladder,snake
+	        System.out.println("check Condition : " +dice2);
+	        if (dice2 == noPlay){
+	            position=dice1;
+	            System.out.println("The position remain same at" +position );
+	        }else if ( dice2 == ladder){
+	            position = num + dice1;
+	            System.out.println("The position received ladder so move forward by " +position );
+	        }else
+	        {
+	            position = num - dice1 ;
+	            if (position < 0){
+	                position=0;}
+	            System.out.println("The position received Snake so move backward by " +position);
+	        }
+
+	        System.out.println("The position number is " +position);
 }
 }
